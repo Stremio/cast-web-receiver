5aff519d03db4b1c6f151bbf25727734dfae0003
Tim <tymmesyde@gmail.com>
2025-12-11 21:20:22 +0100

    test

--- static/main.js
+++ static/main.js
@@ -72,9 +72,9 @@ playerManager.setMessageInterceptor(MESSAGE.LOAD, (request) => {
         console.log('SUPPORTED_VIDEO_CODECS', videoCodecs);
         console.log('SUPPORTED_AUDIO_CODECS', audioCodecs);
 
-        streamUrl.searchParams.append('maxAudioChannels', 2);
-        streamUrl.searchParams.append('maxWidth', 720);
-        streamUrl.searchParams.append('forceTranscoding', 1);
+        // streamUrl.searchParams.append('maxAudioChannels', 2);
+        // streamUrl.searchParams.append('maxWidth', 720);
+        // streamUrl.searchParams.append('forceTranscoding', 1);
 
         request.media.contentId = streamUrl.toString();
     } catch(e) {
